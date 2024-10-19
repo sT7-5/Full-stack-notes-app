a055756bbe10a2956dc808cc03fdd5896c63a72d
Soyam <95719333+sT7-5@users.noreply.github.com>
2024-10-19 22:56:33 +0100

    Removed date feature as it's not working as intended currently

--- frontend/src/components/NoteContent.jsx
+++ frontend/src/components/NoteContent.jsx
@@ -4,7 +4,6 @@ import '../styles/Home.css'
 
 export default function NoteContent(props){
 
-    const theDate = new Date().toLocaleDateString("en-Uk")
 
 
 
@@ -14,7 +13,6 @@ export default function NoteContent(props){
 
         <div >
             <p className="noteContent" >
-                {props.content===''? '' : `Date created: ${theDate}`}
                 <br></br>
                 {props.content}
             </p>
